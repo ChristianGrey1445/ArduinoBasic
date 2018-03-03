6362114e28a9f1500637bd8e29a23ccbf9eb6578
Cristian Martínez <32301608+ChristianGrey1445@users.noreply.github.com>
2018-03-02 22:06:19 -0600

    Add files via upload

---
+++ Arduino_proyect_01.ino
@@ -0,0 +1,15 @@
+//HOLA MUNDO!!
+
+
+void setup() {
+  pinMode(13, OUTPUT); //Declaramos el pin 13 como salida
+
+}
+
+void loop() {
+  digitalWrite(13, HIGH); //Encendemos el led
+  delay(4000);            //Esperamos un segundo
+  digitalWrite(13, LOW);  //Apagamos el led
+  delay(4000);            //Esperamos un segundo
+
+}

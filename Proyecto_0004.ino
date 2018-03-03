04d8831a6ba12f76c862800f4b201cc180574124
Cristian Martínez <32301608+ChristianGrey1445@users.noreply.github.com>
2018-03-02 22:13:35 -0600

    Add files via upload

---
+++ Proyecto_0004.ino
@@ -0,0 +1,28 @@
+//Semaforo de leds
+
+int LED_ROJO=2;
+int LED_AMARILLO=3;
+int LED_VERDE=4;
+
+void setup() {
+  pinMode(LED_ROJO, OUTPUT);
+  pinMode(LED_AMARILLO, OUTPUT);
+  pinMode(LED_VERDE, OUTPUT);
+
+  digitalWrite(LED_ROJO, LOW);
+  digitalWrite(LED_AMARILLO, LOW);
+  digitalWrite(LED_VERDE, LOW);
+}
+
+void loop() {
+  digitalWrite(LED_VERDE, HIGH);
+  delay(3000);
+  digitalWrite(LED_VERDE, LOW);
+  digitalWrite(LED_AMARILLO, HIGH);
+  delay(500);
+   digitalWrite(LED_AMARILLO, LOW);
+  digitalWrite(LED_ROJO, HIGH);
+  delay(2000);
+  digitalWrite(LED_ROJO, LOW);
+
+}
---
+++ Proyecto_0005.ino
@@ -0,0 +1,25 @@
+//Secuencia de leds que prenden y apagan en serie
+
+int timer = 100;
+int matriz []={5, 7, 9, 11, 13};
+int cont=5;
+int pin;
+
+void setup() {
+  for( int pin = 0; pin < cont; pin++){
+    pinMode (matriz [pin], OUTPUT);
+    }
+}
+
+void loop() {
+  for(int pin = 0; pin< cont; pin++ ){
+    digitalWrite (matriz[pin], HIGH);
+    delay(timer);
+    digitalWrite (matriz [pin], LOW);
+    }
+ for(int pin = cont - 1; pin >= 0; pin--){
+    digitalWrite (matriz[pin], HIGH);
+    delay(timer);
+    digitalWrite (matriz [pin], LOW);
+    }
+}
